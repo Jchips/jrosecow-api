d1ba935b547fe87c43c2fb60fed69f86044e0e27
Jchips <20979096+Jchips@users.noreply.github.com>
2023-06-23 23:44:16 -0700

    edited access control link

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://jrosecow.com/protected');
+  res.setHeader('Access-Control-Allow-Origin', 'https://jrosecow.com');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
